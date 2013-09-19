7be20b325dd530d8cef522c2b698a5527811f9d2
James Charlesworth <james.charlesworth@gmail.com>
2013-09-18 21:32:22 -0400

    removed unnecessary controller

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,12 @@
-define(['jquery', 'backbone', 'modules/one/OneController'], function( $, Backbone, OneController ) {
-    
-
-
+define(['jquery', 'backbone', 'modules/one/OneViewA'], function( $, Backbone, OneViewA ) {
     return {
         start: function() {
-           console.log('starting', OneController.start())
+          //OneViewA, requires a model, but that model
+          //is called for within the view itself
+          //it may already be in memory, localStorage (@todo)
+          //or if not, it will fetch from the server
+          var oneViewA = new OneViewA();
+          $('#oneViewA').html(oneViewA.render().el);
         }
     }
 });

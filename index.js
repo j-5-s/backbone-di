de009f614050854174824956756f9bb53ff39922
James Charlesworth <james.charlesworth@gmail.com>
2013-09-22 22:31:31 -0400

    updates node server to say where its located

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ var static = require('node-static');
 // Create a node-static server instance to serve the './public' folder
 //
 var file = new static.Server('./public', {headers: {"Access-Control-Allow-Origin":"*"}});
-
+console.log('Static server listing on port 8080 -> http://localhost:8080');
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        //
-        // Serve files!
-        //
         file.serve(request, response);
     }).resume();
 }).listen(8080);
